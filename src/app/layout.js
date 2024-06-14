fce338e934a7c68d01984cd552367c9f0bb29574
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-06-14 00:19:53 -0500

    Updates for timestamp and hits exercises

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  // for creating timestamp exercise to only read and use from server.
+  const timestamp = new Date().toLocaleString();
+
   return (
     <html lang="en">
       <body>
         {children}
-
+      <footer>Page rendered on {timestamp}</footer>
       </body>
     </html>
   );
